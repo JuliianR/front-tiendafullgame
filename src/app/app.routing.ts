3f320cf0ab9972e8ee20e88a1ab3fd2e1955dd32
Deivid Urrego <coordinacion.tics@invercomes.com.co>
2020-11-24 21:02:15 -0500

    Cargue parametros iniciales del proyecto Se crea el archivo routing y añaden las dependencias de Bootstrap y JQuery

---
+++ src/app/app.routing.ts
@@ -0,0 +1,14 @@
+// IMPORTS NECESARIOS
+
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+// IMPORTAR COMPONENTES
+
+// Definir las rutas
+const appRoutes: Routes = [
+];
+
+// Exportar configuración
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(appRoutes);
